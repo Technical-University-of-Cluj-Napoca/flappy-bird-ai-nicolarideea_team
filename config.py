91a7e038f9e410ca14ccec180fd6f22580d57b66
Larisa Pașca <larisapasca29@gmail.com>
2025-11-27 09:41:37 +0200

    natural selection process

---
+++ config.py
@@ -0,0 +1,10 @@
+import pygame
+import components
+
+window_height = 720
+window_width = 550
+window = pygame.display.set_mode((window_width, window_height))
+
+ground = components.Ground(window_width)
+pipes = []
+import random
